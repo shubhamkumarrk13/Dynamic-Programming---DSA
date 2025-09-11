ce7124c524aa622b5c9f882e7e7b68d278488424
shubhamkumarrk13 <13ishubhamkumar@gmail.com>
2025-09-11 15:35:58 +0530

    Create House Robber - Bottom Up Approach.java

---
+++ House Robber - Bottom Up Approach.java
@@ -0,0 +1,25 @@
+// Problem Link : https://leetcode.com/problems/house-robber/description/
+
+class Solution {
+
+    public int rob(int[] nums) {
+
+        int[] dp = new int[nums.length + 1]; // dp[i] = max. stolen money till ith house
+
+        dp[0] = 0;
+
+        dp[1] = nums[0];
+
+        for(int i=2; i<=nums.length; i++) {
+
+            int steal = nums[i - 1] + dp[i - 2];
+
+            int skip = dp[i - 1];
+
+            dp[i] = Math.max(steal, skip);
+        }
+
+        return dp[nums.length];
+        
+    }
+}
